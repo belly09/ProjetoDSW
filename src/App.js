4828fdd867d01b26017270e8b76c389b3006ec18
Julia Oliveira <silva.julia3@aluno.ifsp.edu.br>
2024-09-24 07:23:52 -0300

    Seção Capa

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Header from './componentes/Header';
+import Produtos from './componentes/Produtos';
 import SecaoCapa from './componentes/SecaoCapa';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <SecaoCapa/>
+      <Produtos/>
     </div>
   );
 }
+
 
 export default App;
